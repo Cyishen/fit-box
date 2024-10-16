f910ac709db6c2b0e503b28885fdac7064c43d7b
cyi <halu0614@gmail.con>
2024-10-16 16:36:48 +0800

    env

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -2,4 +2,4 @@ import { hc } from "hono/client";
 import { AppType } from "@/app/api/[[...route]]/route";
 
 // todo* RPC code (lib folder)
-export const client =  hc<AppType>('http://localhost:3000')
+export const client =  hc<AppType>(process.env.NEXT_PUBLIC_URL!)
