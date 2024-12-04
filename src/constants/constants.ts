b20624d1b4e945665a83a20e07c41061dff55b83
cyi <halu0614@gmail.con>
2024-12-04 17:31:58 +0800

    Fix image name

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -20,15 +20,15 @@ type TemplateExerciseType = {
 }
 
 export const exerciseTemplates: TemplateExerciseType[] = [
-  { movementId: '1', name: '啞鈴胸推', templateSets: [], exerciseCategory: '胸', sortOrder: 1, iconSrc:"/action-imgs/chest-up.png" },
-  { movementId: '2', name: '器械飛鳥夾胸', templateSets: [], exerciseCategory: '胸', sortOrder: 2, iconSrc:"/action-imgs/bird-chest.png" },
-  { movementId: '3', name: '上斜平躺臥推', templateSets: [], exerciseCategory: '胸', sortOrder: 3, iconSrc:"/action-imgs/incline-chest.png" },
-  { movementId: '4', name: '深蹲', templateSets: [], exerciseCategory: '腿', sortOrder: 4, iconSrc:"/action-imgs/squat.png" },
-  { movementId: '5', name: '側平舉', templateSets: [], exerciseCategory: '肩', sortOrder: 5, iconSrc:"/action-imgs/lateral-raise.png" },
-  { movementId: '6', name: '器械側平舉', templateSets: [], exerciseCategory: '肩', sortOrder: 6, iconSrc:"/action-imgs/lateral-raise.png" },
-  { movementId: '7', name: '肩推', templateSets: [], exerciseCategory: '肩', sortOrder: 7, iconSrc:"/action-imgs/push-up.png" },
-  { movementId: '8', name: '悍馬機下拉', templateSets: [], exerciseCategory: '背', sortOrder: 8, iconSrc:"/action-imgs/lat-pull-down.png" },
-  { movementId: '9', name: '器械划船', templateSets: [], exerciseCategory: '背', sortOrder: 9, iconSrc:"/action-imgs/seat-row.png" },
+  { movementId: '1', name: '啞鈴胸推', templateSets: [], exerciseCategory: '胸', sortOrder: 1, iconSrc:"/action-imgs/chest_chest-up.png" },
+  { movementId: '2', name: '器械飛鳥夾胸', templateSets: [], exerciseCategory: '胸', sortOrder: 2, iconSrc:"/action-imgs/chest_bird-chest.png" },
+  { movementId: '3', name: '上斜平躺臥推', templateSets: [], exerciseCategory: '胸', sortOrder: 3, iconSrc:"/action-imgs/chest_incline-chest.png" },
+  { movementId: '4', name: '深蹲', templateSets: [], exerciseCategory: '腿', sortOrder: 4, iconSrc:"/action-imgs/leg_squat.png" },
+  { movementId: '5', name: '側平舉', templateSets: [], exerciseCategory: '肩', sortOrder: 5, iconSrc:"/action-imgs/shoulder_lateral-raise.png" },
+  { movementId: '6', name: '器械側平舉', templateSets: [], exerciseCategory: '肩', sortOrder: 6, iconSrc:"/action-imgs/shoulder_lateral-raise.png" },
+  { movementId: '7', name: '肩推', templateSets: [], exerciseCategory: '肩', sortOrder: 7, iconSrc:"/action-imgs/shoulder_push-up.png" },
+  { movementId: '8', name: '悍馬機下拉', templateSets: [], exerciseCategory: '背', sortOrder: 8, iconSrc:"/action-imgs/back_lat-pull-down.png" },
+  { movementId: '9', name: '器械划船', templateSets: [], exerciseCategory: '背', sortOrder: 9, iconSrc:"/action-imgs/back_seat-row.png" },
 ];
 
 type WorkoutExerciseType = {
@@ -43,15 +43,15 @@ type WorkoutExerciseType = {
 };
 
 export const exerciseWorkouts: WorkoutExerciseType[] = [
-  { movementId: '1', name: '啞鈴胸推', sets: [], exerciseCategory: '胸', workoutSessionId: '', iconSrc:"/action-imgs/chest-up.png" },
-  { movementId: '2', name: '器械飛鳥夾胸', sets: [], exerciseCategory: '胸', workoutSessionId: '', iconSrc:"/action-imgs/bird-chest.png" },
-  { movementId: '3', name: '上斜平躺臥推', sets: [], exerciseCategory: '胸', workoutSessionId: '', iconSrc:"/action-imgs/incline-chest.png" },
-  { movementId: '4', name: '深蹲', sets: [], exerciseCategory: '腿', workoutSessionId: '', iconSrc:"/action-imgs/squat.png" },
-  { movementId: '5', name: '側平舉', sets: [], exerciseCategory: '肩', workoutSessionId: '', iconSrc:"/action-imgs/lateral-raise.png" },
-  { movementId: '6', name: '器械側平舉', sets: [], exerciseCategory: '肩', workoutSessionId: '', iconSrc:"/action-imgs/lateral-raise.png" },
-  { movementId: '7', name: '肩推', sets: [], exerciseCategory: '肩', workoutSessionId: '', iconSrc:"/action-imgs/push-up.png" },
-  { movementId: '8', name: '悍馬機下拉', sets: [], exerciseCategory: '背', workoutSessionId: '', iconSrc:"/action-imgs/lat-pull-down.png" },
-  { movementId: '9', name: '器械划船', sets: [], exerciseCategory: '背', workoutSessionId: '', iconSrc:"/action-imgs/seat-row.png" },
+  { movementId: '1', name: '啞鈴胸推', sets: [], exerciseCategory: '胸', workoutSessionId: '', iconSrc:"/action-imgs/chest_chest-up.png" },
+  { movementId: '2', name: '器械飛鳥夾胸', sets: [], exerciseCategory: '胸', workoutSessionId: '', iconSrc:"/action-imgs/chest_bird-chest.png" },
+  { movementId: '3', name: '上斜平躺臥推', sets: [], exerciseCategory: '胸', workoutSessionId: '', iconSrc:"/action-imgs/chest_incline-chest.png" },
+  { movementId: '4', name: '深蹲', sets: [], exerciseCategory: '腿', workoutSessionId: '', iconSrc:"/action-imgs/leg_squat.png" },
+  { movementId: '5', name: '側平舉', sets: [], exerciseCategory: '肩', workoutSessionId: '', iconSrc:"/action-imgs/shoulder_lateral-raise.png" },
+  { movementId: '6', name: '器械側平舉', sets: [], exerciseCategory: '肩', workoutSessionId: '', iconSrc:"/action-imgs/shoulder_lateral-raise.png" },
+  { movementId: '7', name: '肩推', sets: [], exerciseCategory: '肩', workoutSessionId: '', iconSrc:"/action-imgs/shoulder_push-up.png" },
+  { movementId: '8', name: '悍馬機下拉', sets: [], exerciseCategory: '背', workoutSessionId: '', iconSrc:"/action-imgs/back_lat-pull-down.png" },
+  { movementId: '9', name: '器械划船', sets: [], exerciseCategory: '背', workoutSessionId: '', iconSrc:"/action-imgs/back_seat-row.png" },
 ];
 
 
