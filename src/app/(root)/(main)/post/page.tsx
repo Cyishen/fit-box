bc508513dbfc38c84fe8c67dae8c033ee39d9c71
cyi <halu0614@gmail.con>
2024-12-25 16:01:52 +0800

    fix

--- src/app/(root)/(main)/post/page.tsx
+++ src/app/(root)/(main)/post/page.tsx
@@ -5,7 +5,7 @@ import PostRef from './PostRef'
 
 const PostPage = () => {
   return (
-    <section className='flex flex-col bg-white sm:bg-[#f3f2f8] h-dvh'>
+    <section className='flex flex-col bg-white sm:bg-[#f3f2f8] h-full'>
       <PostRef />
       <div className='h-full mx-auto w-full max-w-screen-sm overflow-y-scroll sm:rounded-t-3xl sm:border-[0.5px] custom-scrollbar'>
         <PostList />
--- src/components/mobile/FooterVisibility.tsx
+++ src/components/mobile/FooterVisibility.tsx
@@ -3,14 +3,48 @@
 import MobileFooter from "@/components/mobile/MobileFooter";
 import { usePathname } from "next/navigation";
 
+import { useScroll, useMotionValueEvent } from "framer-motion";
+import { useRef, useState } from "react";
+
+
 const FooterVisibility = () => {
   const pathname = usePathname();
-
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
   // 定義主要路由
   const mainRoutes = ["/fit", "/action", "/record", "/profile", "/post"];
   const shouldShowFooter = mainRoutes.includes(pathname);
 
-  return shouldShowFooter ? <MobileFooter /> : null;
+  const { scrollY } = useScroll()
+  const [scrolled, setScrolled] = useState(false)
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest === 0) {
+      setScrolled(false);
+    } else if (latest > lastScrollY) {
+      setScrolled(true);
+    } else if (latest < lastScrollY) {
+      setScrolled(false);
+    }
+
+    setLastScrollY(latest);
+
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    timerRef.current = setTimeout(() => {
+      setScrolled(false);
+    }, 500); 
+  })
+
+  return (
+    <div>
+      {shouldShowFooter ? (
+        scrolled ? null : <MobileFooter />
+      ) : null
+      }
+    </div>
+  );
 };
 
 export default FooterVisibility;
